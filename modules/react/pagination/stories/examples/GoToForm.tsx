4bbb056a50ae776caf9a69e2e69434c034200048
William Stanton <40372497+williamjstanton@users.noreply.github.com>
2023-09-19 14:19:31 -0700

    Update GoToForm.tsx
    Enhancing with Tooltip, and using aria-describedby on the text input to reference the label.

--- modules/react/pagination/stories/examples/GoToForm.tsx
+++ modules/react/pagination/stories/examples/GoToForm.tsx
@@ -5,11 +5,14 @@ import {
   getVisibleResultsMax,
   getVisibleResultsMin,
 } from '@workday/canvas-kit-react/pagination';
+import {useUniqueId} from '@workday/canvas-kit-react/common';
+import {Tooltip} from '../../../tooltip';
 
 export const GoToForm = () => {
   const resultCount = 10;
   const totalCount = 100;
   const lastPage = getLastPage(resultCount, totalCount);
+  const myId = useUniqueId();
 
   return (
     <Pagination
@@ -18,22 +21,36 @@ export const GoToForm = () => {
       lastPage={lastPage}
     >
       <Pagination.Controls>
-        <Pagination.JumpToFirstButton aria-label="First" />
-        <Pagination.StepToPreviousButton aria-label="Previous" />
+        <Tooltip title="First">
+          <Pagination.JumpToFirstButton />
+        </Tooltip>
+        <Tooltip title="Previous">
+          <Pagination.StepToPreviousButton />
+        </Tooltip>
         <Pagination.PageList>
           {({state}) =>
             state.range.map(pageNumber => (
               <Pagination.PageListItem key={pageNumber}>
-                <Pagination.PageButton aria-label={`Page ${pageNumber}`} pageNumber={pageNumber} />
+                <Tooltip title={`Page ${pageNumber}`}>
+                  <Pagination.PageButton pageNumber={pageNumber} />
+                </Tooltip>
               </Pagination.PageListItem>
             ))
           }
         </Pagination.PageList>
-        <Pagination.StepToNextButton aria-label="Next" />
-        <Pagination.JumpToLastButton aria-label="Last" />
+        <Tooltip title="Next">
+          <Pagination.StepToNextButton />
+        </Tooltip>
+        <Tooltip title="Last">
+          <Pagination.JumpToLastButton />
+        </Tooltip>
         <Pagination.GoToForm>
-          <Pagination.GoToTextInput aria-label="Go to page number" />
-          <Pagination.GoToLabel>{({state}) => `of ${state.lastPage} pages`}</Pagination.GoToLabel>
+          <Tooltip title="Go to page">
+            <Pagination.GoToTextInput aria-label="Go to page" aria-describedby={myId} />
+          </Tooltip>
+          <Pagination.GoToLabel id={myId}>
+            {({state}) => `of ${state.lastPage} pages`}
+          </Pagination.GoToLabel>
         </Pagination.GoToForm>
       </Pagination.Controls>
       <Pagination.AdditionalDetails shouldHideDetails>
