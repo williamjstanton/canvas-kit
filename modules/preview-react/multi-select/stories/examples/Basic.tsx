da132b07f562c51dab7044f69cdf3a3aaa898269
William Stanton <william.stanton@workday.com>
2024-10-30 18:26:31 -0400

    docs: Creating super status on multi select example

--- modules/preview-react/multi-select/stories/examples/Basic.tsx
+++ modules/preview-react/multi-select/stories/examples/Basic.tsx
@@ -2,15 +2,40 @@ import React from 'react';
 
 import {FormField} from '@workday/canvas-kit-react/form-field';
 import {MultiSelect} from '@workday/canvas-kit-preview-react/multi-select';
+import {StatusIndicator} from '@workday/canvas-kit-preview-react/status-indicator';
+import {Dialog} from '@workday/canvas-kit-react/dialog';
 
 const items = ['Cheese', 'Olives', 'Onions', 'Pepperoni', 'Peppers'];
+
+const SuperStatus = () => {
+  return (
+    <StatusIndicator emphasis="high">
+      <StatusIndicator.Label>Modified</StatusIndicator.Label>
+      <Dialog>
+        <Dialog.Target>Details</Dialog.Target>
+        <Dialog.Popper>
+          <Dialog.Card>
+            <Dialog.CloseIcon aria-label="Close" />
+            <Dialog.Heading paddingTop="m">Delta View</Dialog.Heading>
+            <Dialog.Body>
+              <p>body content</p>
+            </Dialog.Body>
+          </Dialog.Card>
+        </Dialog.Popper>
+      </Dialog>
+    </StatusIndicator>
+  );
+};
 
 export const Basic = () => {
   return (
     <>
       <MultiSelect items={items}>
-        <FormField orientation="horizontal">
-          <FormField.Label>Toppings</FormField.Label>
+        <FormField orientation="vertical">
+          <FormField.Label>
+            Toppings
+            <SuperStatus />
+          </FormField.Label>
           <FormField.Input as={MultiSelect.Input} placeholder="Select Multiple" />
           <MultiSelect.Popper>
             <MultiSelect.Card>
