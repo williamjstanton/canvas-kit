f63e41bf2b03a9eb4ddd6da4ca948717579e1997
William Stanton <william.stanton@workday.com>
2024-11-01 12:20:05 -0400

    docs: Improving styles and building popup example

--- modules/preview-react/multi-select/stories/examples/Basic.tsx
+++ modules/preview-react/multi-select/stories/examples/Basic.tsx
@@ -4,6 +4,10 @@ import {FormField} from '@workday/canvas-kit-react/form-field';
 import {MultiSelect} from '@workday/canvas-kit-preview-react/multi-select';
 import {StatusIndicator} from '@workday/canvas-kit-preview-react/status-indicator';
 import {Dialog} from '@workday/canvas-kit-react/dialog';
+import {TertiaryButton} from '@workday/canvas-kit-react/button';
+import {BodyText} from '@workday/canvas-kit-react/text';
+import {SystemIcon} from '@workday/canvas-kit-react/icon';
+import {caretBottomSmallIcon, plusCircleIcon, xCircleIcon} from '@workday/canvas-system-icons-web';
 
 const items = ['Cheese', 'Olives', 'Onions', 'Pepperoni', 'Peppers'];
 
@@ -12,13 +16,21 @@ const SuperStatus = () => {
     <StatusIndicator emphasis="high">
       <StatusIndicator.Label>Modified</StatusIndicator.Label>
       <Dialog>
-        <Dialog.Target>Details</Dialog.Target>
+        <Dialog.Target as={TertiaryButton} icon={caretBottomSmallIcon} variant="inverse" />
         <Dialog.Popper>
           <Dialog.Card>
             <Dialog.CloseIcon aria-label="Close" />
-            <Dialog.Heading paddingTop="m">Delta View</Dialog.Heading>
+            <Dialog.Heading>Delta View</Dialog.Heading>
             <Dialog.Body>
-              <p>body content</p>
+              <BodyText size="small">
+                <SystemIcon icon={xCircleIcon} />
+                https://jira2.workday.com/secure/attachment/5968514/Screen%20Recording%202024-07-16%20at%2019.44.18.mov
+                Mushrooms
+              </BodyText>
+              <BodyText size="small">
+                <SystemIcon icon={xCircleIcon} />
+                Olives
+              </BodyText>
             </Dialog.Body>
           </Dialog.Card>
         </Dialog.Popper>
