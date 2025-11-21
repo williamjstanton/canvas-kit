d7eb30499dd7a87c048c3b7892aa247574012337
William Stanton <william.stanton@workday.com>
2025-11-21 17:03:54 -0500

    docs: Refactoring Initial Focus example

--- modules/react/popup/stories/examples/InitialFocus.tsx
+++ modules/react/popup/stories/examples/InitialFocus.tsx
@@ -7,10 +7,28 @@ import {
   usePopupModel,
   useInitialFocus,
   useReturnFocus,
+  useFocusRedirect,
 } from '@workday/canvas-kit-react/popup';
 import {Box, Flex} from '@workday/canvas-kit-react/layout';
+import {createStyles, px2rem} from '@workday/canvas-kit-styling';
+import {system} from '@workday/canvas-tokens-web';
+import {useUniqueId} from '@workday/canvas-kit-react/common';
+
+const cardStyles = createStyles({
+  width: px2rem(400),
+});
+
+const bodyStyles = createStyles({
+  marginY: system.space.zero,
+});
+
+const flexStyles = createStyles({
+  gap: system.space.x4,
+  padding: system.space.x2,
+});
 
 export const InitialFocus = () => {
+  const messageId = useUniqueId();
   const initialFocusRef = React.useRef(null);
   const model = usePopupModel({
     initialFocusRef,
@@ -20,23 +38,21 @@ export const InitialFocus = () => {
   useCloseOnEscape(model);
   useInitialFocus(model);
   useReturnFocus(model);
+  useFocusRedirect(model);
 
   return (
     <Popup model={model}>
       <Popup.Target>Send Message</Popup.Target>
       <Popup.Popper placement={'bottom'}>
-        <Popup.Card width={400}>
-          <Popup.CloseIcon aria-label="Close" />
+        <Popup.Card cs={cardStyles} aria-describedby={messageId}>
           <Popup.Heading>Confirmation</Popup.Heading>
           <Popup.Body>
-            <Box as="p" marginY="zero" id="popup-message">
+            <Box as="p" cs={bodyStyles} id={messageId}>
               Your message has been sent!
             </Box>
           </Popup.Body>
-          <Flex gap="s" padding="xxs">
-            <Popup.CloseButton ref={initialFocusRef} aria-describedby="popup-message">
-              OK
-            </Popup.CloseButton>
+          <Flex cs={flexStyles}>
+            <Popup.CloseButton ref={initialFocusRef}>OK</Popup.CloseButton>
           </Flex>
         </Popup.Card>
       </Popup.Popper>
